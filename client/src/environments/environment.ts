693d00a84fee0d56f56249c1b248c4bb44edae90
hoanggaphan <phansihoang1998@gmail.com>
2023-11-22 18:03:35 +0700

    fix deploy

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'server-pick-location',
-  apiUrl: 'server-pick-location/api',
+  serverUrl: 'pick-location-server',
+  apiUrl: 'pick-location-server/api',
   ggApiKey: 'AIzaSyDbUHqvgfL_LgqAW_tYZQB-cq8GiAeEm8Q',
 };
