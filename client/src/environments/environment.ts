8acb919447ac5afaa894cf7454fef1d7464b399d
hoanggaphan <phansihoang1998@gmail.com>
2023-11-22 18:40:30 +0700

    fix url for deploy client

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://pick-location-server',
-  apiUrl: 'https://pick-location-server/api',
+  serverUrl: 'https://pick-location-server.vercel.app',
+  apiUrl: 'https://pick-location-server.vercel.app/api',
   ggApiKey: 'AIzaSyDbUHqvgfL_LgqAW_tYZQB-cq8GiAeEm8Q',
 };
