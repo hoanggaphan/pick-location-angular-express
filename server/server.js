9a596313f5951887247202bddcc06f397fcfbf1d
hoanggaphan <phansihoang1998@gmail.com>
2023-11-22 22:37:04 +0700

    fix to deploy

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || '*',
+    methods: ['GET', 'POST'],
     credentials: true,
   },
 });
