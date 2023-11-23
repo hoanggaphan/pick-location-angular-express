afb93d667b0b2e1ee3f975d828de0e77c069fd58
hoanggaphan <phansihoang1998@gmail.com>
2023-11-23 15:46:45 +0700

    change ggapikey from client

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   serverUrl: 'http://localhost:3000',
   apiUrl: 'http://localhost:3000/api',
-  ggApiKey: 'AIzaSyDbUHqvgfL_LgqAW_tYZQB-cq8GiAeEm8Q',
+  ggApiKey: 'createFromConsoleGoole',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   serverUrl: 'https://pick-location-server.vercel.app',
   apiUrl: 'https://pick-location-server.vercel.app/api',
-  ggApiKey: 'AIzaSyDbUHqvgfL_LgqAW_tYZQB-cq8GiAeEm8Q',
+  ggApiKey: 'createFromConsoleGoole',
 };
