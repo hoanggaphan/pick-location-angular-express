f0150818a84eef94b966219b2527aa187b31671a
hoanggaphan <phansihoang1998@gmail.com>
2023-11-23 12:48:43 +0700

    fix cors

--- server/server.js
+++ server/server.js
@@ -16,8 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || '*',
-    credentials: true,
+    origin: '*',
   },
 });
 
